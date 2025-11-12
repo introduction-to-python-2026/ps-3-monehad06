e74afd63b5e33192e60eae1b4636d971a937a7e9
monehad06 <mo.nehad06@gmail.com>
2025-11-12 12:53:48 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,15 +1,14 @@
-def move(my_list, direction):
 
-    # Finds the index of the one in the list
+    def move(my_list, direction):
     index_of_one = my_list.index(1)
 
-    # Move the one to the left or to the right
-    if direction == 'right':
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
+    if direction == "right":
+        if index_of_one != len(my_list) - 1:
+            my_list[index_of_one], my_list[index_of_one + 1] = 0, 1
 
-    elif direction == 'left':
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
+    elif direction == "left":
+        if index_of_one != 0:
+            my_list[index_of_one], my_list[index_of_one - 1] = 0, 1
 
     return my_list
+
